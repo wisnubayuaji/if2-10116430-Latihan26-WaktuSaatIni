cbadd123e2fec99cd3314d756d964f41868b9f35
wisnubayuaji <wisnubayu34@gmail.com>
2020-11-01 07:29:53 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+/**
+ * @author
+ * Nim  : 10116430
+ * Nama : Wisnu Bayu Aji
+ * Kelas: IF-2
+ */
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int detik, menit, jam, hari, bulan, tahun;
+        GregorianCalendar date = new GregorianCalendar();
+        String namabulan[] = { "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September",
+                "Oktober", "November", "Desember" };
+        detik = date.get(Calendar.SECOND);
+        menit = date.get(Calendar.MINUTE);
+        jam = date.get(Calendar.HOUR_OF_DAY);
+        hari = date.get(Calendar.DAY_OF_MONTH);
+        bulan = date.get(Calendar.MONTH);
+        tahun = date.get(Calendar.YEAR);
+        System.out.println("Hari ini adalah hari : " + hari + " " + namabulan[bulan] + " " + tahun + " " 
+            + jam + ":" + menit + ":" + detik);
     }
 }
